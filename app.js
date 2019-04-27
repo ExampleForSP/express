189e70ae0bd7ccec0e3cb39518eb5b6793e683a1
Bartek Borowczyk <bartek@websamuraj.pl>
2019-04-27 17:24:46 +0200

    admin login, admin news

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 var createError = require('http-errors');
+var cookieSession = require('cookie-session');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var config = require('./config');
+var mongoose = require('mongoose');
+
+mongoose.connect(config.db, { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
 
 var indexRouter = require('./routes/index');
 var newsRouter = require('./routes/news');
@@ -20,6 +28,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cookieSession({
+  name: 'session',
+  keys: config.keySession,
+  maxAge: config.maxAgeSession
+}))
 
 app.use(function (req, res, next) {
   res.locals.path = req.path;
---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  db: '',
+  keySession: ['TWOJKLUCZ'],
+  maxAgeSession: 24 * 60 * 60 * 1000
+}
