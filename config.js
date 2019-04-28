9e9b5e6f80d51120f1225d509dec5edb210f7a06
Bartek Borowczyk <bartek@websamuraj.pl>
2019-04-28 10:44:11 +0200

    config db

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  db: '',
+  db: 'mongodb+srv://admin:p6tdGLXJBWOOOYSy@cluster0-0dwhe.mongodb.net/test?retryWrites=true',
   keySession: ['TWOJKLUCZ'],
   maxAgeSession: 24 * 60 * 60 * 1000
 }
